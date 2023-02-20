b31423d282b800ba157a0e6bce2ceba20a311964
leedongyeop <ldy_1204@naver.com>
2023-02-20 22:41:01 +0900

    feat: 셔틀버스 정보 엔티티 설계 및 관계 매핑

---
+++ src/main/java/io/wisoft/capstonedesign/domain/BusInfo.java
@@ -0,0 +1,16 @@
+package io.wisoft.capstonedesign.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class BusInfo {
+
+    @Id @GeneratedValue
+    @Column(name = "bus_info_id")
+    private Long id;
+
+    @Column(name = "bus_info_path", nullable = false)
+    private String busInfoPath;
+}
