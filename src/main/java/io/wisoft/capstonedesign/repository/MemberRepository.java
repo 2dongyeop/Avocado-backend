4813f81431ae2c5d3a3aaba401b3ed57864578ce
leedongyeop <ldy_1204@naver.com>
2023-03-01 19:01:04 +0900

    feat: 회원 서비스 및 레포지토리 기능 개발

---
+++ src/main/java/io/wisoft/capstonedesign/repository/MemberRepository.java
@@ -0,0 +1,47 @@
+package io.wisoft.capstonedesign.repository;
+
+import io.wisoft.capstonedesign.domain.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 회원가입 : signUp
+     */
+    public void signUp(Member member) {
+        em.persist(member);
+    }
+
+    /**
+     * 로그인 : login
+     */
+    public void login(Member member) {
+
+    }
+
+    /**
+     * 회원조회
+     */
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByEmail(String email) {
+        return em.createQuery("select m from Member m where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getResultList();
+    }
+}
