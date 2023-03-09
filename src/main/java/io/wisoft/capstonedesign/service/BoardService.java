3c4a89e5f9977944317056d7b8bbead6d3ce279c
leedongyeop <ldy_1204@naver.com>
2023-03-09 23:05:06 +0900

    refactor : repository에 작성했던 null 체크 로직을 service로 옮김

--- src/main/java/io/wisoft/capstonedesign/service/BoardService.java
+++ src/main/java/io/wisoft/capstonedesign/service/BoardService.java
@@ -1,6 +1,7 @@
 package io.wisoft.capstonedesign.service;
 
 import io.wisoft.capstonedesign.domain.Board;
+import io.wisoft.capstonedesign.exception.nullcheck.NullBoardException;
 import io.wisoft.capstonedesign.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -36,7 +37,14 @@ public class BoardService {
     /* 조회 로직 */
     public List<Board> findByMemberId(Long memberId) { return boardRepository.findByMemberId(memberId); }
 
-    public Board findOne(Long boardId) { return boardRepository.findOne(boardId); }
+    public Board findOne(Long boardId) {
+        Board getBoard = boardRepository.findOne(boardId);
+
+        if (getBoard == null) {
+            throw new NullBoardException("해당 게시글은 존재하지 않습니다.");
+        }
+        return getBoard;
+    }
 
     public List<Board> findAll() { return boardRepository.findAll(); }
 }
