35f06b07a96517dc6db6355e999eedb60fdfbb6f
leedongyeop <ldy_1204@naver.com>
2023-03-11 23:48:17 +0900

    feat: 의료진 조회 기능 추가 - 자신이 속한 병원 리뷰 목록 조회, 자신이 댓글을 작성한 게시글 목록 조회

--- src/main/java/io/wisoft/capstonedesign/service/StaffService.java
+++ src/main/java/io/wisoft/capstonedesign/service/StaffService.java
@@ -1,10 +1,13 @@
 package io.wisoft.capstonedesign.service;
 
 
+import io.wisoft.capstonedesign.domain.Board;
 import io.wisoft.capstonedesign.domain.Hospital;
+import io.wisoft.capstonedesign.domain.Review;
 import io.wisoft.capstonedesign.domain.Staff;
 import io.wisoft.capstonedesign.domain.enumeration.HospitalDept;
 import io.wisoft.capstonedesign.exception.duplicate.DuplicateStaffException;
+import io.wisoft.capstonedesign.exception.nullcheck.NullStaffException;
 import io.wisoft.capstonedesign.repository.StaffRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -57,9 +60,39 @@ public class StaffService {
     }
 
     /**
+     * 자신이 속한 병원의 리뷰 목록 조회
+     */
+    public List<Review> findReviewByStaffHospitalName(Long staffId) {
+
+        Staff staff = staffRepository.findOne(staffId);
+        String hospitalName = staff.getHospital().getName();
+
+        return staffRepository.findReviewListByStaffHospitalName(hospitalName);
+    }
+
+    /**
+     * 자신이 댓글 단 게시글 목록 조회
+     */
+    public List<Board> findBoardListByStaffId(Long staffId) {
+
+        Staff staff = findOne(staffId);
+
+        return staffRepository.findBoardListByStaffId(staff);
+    }
+
+
+    /**
      * 의료진 조회
      */
-    public Staff findOne(Long staffId) { return staffRepository.findOne(staffId); }
+    public Staff findOne(Long staffId) {
+
+        Staff getStaff = staffRepository.findOne(staffId);
+
+        if (getStaff == null) {
+            throw new NullStaffException("해당 의료진 정보가 존재하지 않습니다.");
+        }
+        return getStaff;
+    }
 
     public List<Staff> findAll() { return staffRepository.findAll(); }
 }
