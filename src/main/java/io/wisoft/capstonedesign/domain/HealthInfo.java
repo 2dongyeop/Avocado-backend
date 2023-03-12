9ae73d9b1d7f4d1a50590b94d739941a74b38bf4
leedongyeop <ldy_1204@naver.com>
2023-03-12 16:21:23 +0900

    feat: 정적 생성 메서드 추가 및 건강정보 삭제 기능 추가

--- src/main/java/io/wisoft/capstonedesign/domain/HealthInfo.java
+++ src/main/java/io/wisoft/capstonedesign/domain/HealthInfo.java
@@ -1,12 +1,18 @@
 package io.wisoft.capstonedesign.domain;
 
+import io.wisoft.capstonedesign.domain.enumeration.HealthInfoStatus;
+import io.wisoft.capstonedesign.domain.enumeration.HospitalDept;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
+@Getter
 @Table(name = "health_info")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HealthInfo {
 
     @Id @GeneratedValue()
@@ -15,4 +21,44 @@ public class HealthInfo {
 
     @Column(name = "health_info_path", nullable = false)
     private String healthInfoPath;
+
+    @Column(name = "health_info_status")
+    @Enumerated(value = EnumType.STRING)
+    private HealthInfoStatus status;
+
+    @Column(name = "health_info_title", nullable = false)
+    private String title;
+
+    @Column(name = "dept")
+    @Enumerated(value = EnumType.STRING)
+    private HospitalDept dept;
+
+    @Column(name = "health_info_create_at")
+    private LocalDateTime createAt;
+
+    /* 정적 생성 메서드 */
+    public static HealthInfo createHealthInfo(String healthInfoPath, String title, HospitalDept dept) {
+
+        HealthInfo healthInfo = new HealthInfo();
+        healthInfo.healthInfoPath = healthInfoPath;
+        healthInfo.title = title;
+        healthInfo.dept = dept;
+
+        healthInfo.status = HealthInfoStatus.WRITE;
+        healthInfo.createAt = LocalDateTime.now();
+
+        return healthInfo;
+    }
+
+    /**
+     * 건강정보 삭제
+     */
+    public void delete() {
+
+        if (this.status == HealthInfoStatus.DELETE) {
+            throw new IllegalStateException("이미 삭제된 건강정보입니다.");
+        }
+
+        this.status = HealthInfoStatus.DELETE;
+    }
 }
