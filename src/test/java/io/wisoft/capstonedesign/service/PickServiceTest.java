8c6cd3293ce897aec87f41de5ff0c2f0d62408b0
leedongyeop <ldy_1204@naver.com>
2023-03-20 20:43:01 +0900

    feat: 찜하기 기능 API 개발

--- src/test/java/io/wisoft/capstonedesign/service/PickServiceTest.java
+++ src/test/java/io/wisoft/capstonedesign/service/PickServiceTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 import static org.junit.Assert.*;
 
