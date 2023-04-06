6ff6a996af2a07ec5016a351b6b82e45e6fb3e35
leedongyeop <ldy_1204@naver.com>
2023-04-06 13:43:55 +0900

    refactor: Jbcrypt 라이브러리의 bcrypt를 빈으로 등록

--- src/main/java/io/wisoft/capstonedesign/config/BcryptConfig.java
+++ src/main/java/io/wisoft/capstonedesign/config/BcryptConfig.java
@@ -2,14 +2,11 @@ package io.wisoft.capstonedesign.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BcryptConfig {
-
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    EncryptHelper encryptHelper() {
+        return new BCryptEncoder();
     }
 }
