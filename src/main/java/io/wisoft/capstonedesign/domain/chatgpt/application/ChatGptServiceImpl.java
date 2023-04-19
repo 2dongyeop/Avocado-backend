fb50e5c11b0af941034ccde9d502f1190b68158d
leedongyeop <ldy_1204@naver.com>
2023-04-19 23:09:58 +0900

    style: interceptor 추가 등록 및 로깅

--- src/main/java/io/wisoft/capstonedesign/domain/chatgpt/application/ChatGptServiceImpl.java
+++ src/main/java/io/wisoft/capstonedesign/domain/chatgpt/application/ChatGptServiceImpl.java
@@ -4,6 +4,7 @@ import io.wisoft.capstonedesign.domain.chatgpt.web.dto.ChatGptRequest;
 import io.wisoft.capstonedesign.domain.chatgpt.web.dto.ChatGptResponse;
 import io.wisoft.capstonedesign.domain.chatgpt.web.dto.ChatRequest;
 import io.wisoft.capstonedesign.global.config.ChatGptConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,13 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class ChatGptServiceImpl implements ChatGptService {
     private static final RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public ChatGptResponse askQuestion(final ChatRequest chatRequest) {
-        return this.getResponse(
+        ChatGptResponse response = this.getResponse(
                 this.buildHttpEntity(
                         new ChatGptRequest(
                                 ChatGptConfig.MODEL,
@@ -25,11 +27,15 @@ public class ChatGptServiceImpl implements ChatGptService {
                                 ChatGptConfig.MAX_TOKEN,
                                 ChatGptConfig.TEMPERATURE,
                                 ChatGptConfig.TOP_P)));
+
+        log.info("OpenAI 요청이 들어왔습니다 : {} ", chatRequest.message());
+        log.info("답변 : {}", response.choices().get(0).text());
+        return response;
     }
 
 
     //comment: Build headers
-    public HttpEntity<ChatGptRequest> buildHttpEntity(final ChatGptRequest chatGptRequest) {
+    private HttpEntity<ChatGptRequest> buildHttpEntity(final ChatGptRequest chatGptRequest) {
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE));
         headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + ChatGptConfig.API_KEY);
@@ -37,7 +43,7 @@ public class ChatGptServiceImpl implements ChatGptService {
     }
 
     //comment: Generate response
-    public ChatGptResponse getResponse(final HttpEntity<ChatGptRequest> chatGptRequestHttpEntity) {
+    private ChatGptResponse getResponse(final HttpEntity<ChatGptRequest> chatGptRequestHttpEntity) {
         final ResponseEntity<ChatGptResponse> responseEntity = restTemplate.postForEntity(
                 ChatGptConfig.URL,
                 chatGptRequestHttpEntity,
