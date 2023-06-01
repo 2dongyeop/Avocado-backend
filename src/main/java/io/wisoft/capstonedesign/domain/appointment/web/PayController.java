9dcd99a591f6bb2a304a142e313b0fb6005cd5c5
leedongyeop <ldy_1204@naver.com>
2023-06-01 20:46:26 +0900

    feat: 결제 요청을 보내는 프론트 화면을 임시로 타임리프로 작성

--- src/main/java/io/wisoft/capstonedesign/domain/appointment/web/PayController.java
+++ src/main/java/io/wisoft/capstonedesign/domain/appointment/web/PayController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,6 +31,12 @@ public class PayController {
     @Value("${iamport.api-secret}")
     private String API_SECRET;
 //    private final String API_SECRET = IamportConfig.API_SECRET;
+
+    @GetMapping("/payment")
+    public String payment() {
+        return "payment";
+    }
+
 
     @PostMapping("/payment/callback")
     public ResponseEntity<?> callbackReceived(@RequestBody final Map<String, Object> model) {
