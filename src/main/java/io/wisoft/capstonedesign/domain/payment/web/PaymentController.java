91b21b034263aaf1525a7644008bafefe53f8b9c
leedongyeop <ldy_1204@naver.com>
2023-06-07 19:59:10 +0900

    feat : 예약 취소시 필요한 토큰을 얻는 로직

--- src/main/java/io/wisoft/capstonedesign/domain/payment/web/PaymentController.java
+++ src/main/java/io/wisoft/capstonedesign/domain/payment/web/PaymentController.java
@@ -11,6 +11,9 @@ import io.wisoft.capstonedesign.global.enumeration.status.PayStatus;
 import io.wisoft.capstonedesign.global.exception.illegal.IllegalValueException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,7 +23,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 @Slf4j
@@ -43,7 +48,7 @@ public class PaymentController {
     }
 
 
-    @PostMapping("/payment/callback")
+    @PostMapping("/payment")
     public ResponseEntity<?> callbackReceived(@RequestBody final Map<String, Object> model) {
 
         //응답 header 생성
@@ -75,6 +80,97 @@ public class PaymentController {
             throw new RuntimeException(e);
         }
     }
+
+
+//    @PostMapping("/refund")
+//    public ResponseEntity<?> refundReceived(@RequestBody final Map<String, Object> model) {
+//
+//
+//    }
+
+
+    @GetMapping("/token")
+    public ResponseEntity<?> getToken() throws IOException, JSONException {
+
+        final HttpURLConnection conn = getHttpURLConnection();
+
+        final JSONObject obj = getJsonObject();
+
+        sendRequest(conn, obj);
+
+        final int responseCode = getResponseCode(conn);
+
+        if (responseCode != 200) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        final BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        getResponse(br);
+
+        disconnect(conn, br);
+        return ResponseEntity.ok().build();
+    }
+
+    private void disconnect(final HttpURLConnection conn, final BufferedReader br) throws IOException {
+        br.close();
+        conn.disconnect();
+    }
+
+    @NotNull
+    private StringBuilder getResponse(final BufferedReader br) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+
+        System.out.println(sb);
+        return sb;
+    }
+
+    private int getResponseCode(final HttpURLConnection conn) throws IOException {
+        final int responseCode = conn.getResponseCode();
+        System.out.println("responseCode = " + responseCode);
+
+        return responseCode;
+    }
+
+    private void sendRequest(final HttpURLConnection conn, final JSONObject obj) throws IOException {
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+        bw.write(obj.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    @NotNull
+    private JSONObject getJsonObject() throws JSONException {
+
+        //자신의 키를 JSON 형태로 변환
+        final JSONObject obj = new JSONObject();
+        obj.put("imp_key", API_KEY);
+        obj.put("imp_secret", API_SECRET);
+        return obj;
+    }
+
+    @NotNull
+    private HttpURLConnection getHttpURLConnection() throws IOException {
+        final URL url = new URL("https://api.iamport.kr/users/getToken");
+        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        //요청방식 : POST
+        conn.setRequestMethod("POST");
+
+        //Header 설정
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+
+        //Data 설정 - OutputStream으로 데이터를 넘기겠다
+        conn.setDoOutput(true);
+        return conn;
+    }
+
 
     private void validateAppointmentPayStatus(final Long appointmentId) {
         final Appointment appointment = appointmentService.findById(appointmentId);
